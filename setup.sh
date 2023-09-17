3045f16cab4b3e5431955a4bb31f812d4de50654
Nick Strauch <61529274+NickStrauch13@users.noreply.github.com>
2023-09-17 17:09:05 +0000

    Added path in dockerfile

--- setup.sh
+++ setup.sh
@@ -2,5 +2,4 @@
 source /home/vscode/venv/bin/activate
 # Append it to bash so every shell launches with it 
 echo 'source /home/vscode/venv/bin/activate' >> ~/.bashrc
-# Add the src folder to the python path
-export PYTHONPATH="$PWD/src"
+
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,4 +1,4 @@
-from src.main import func
+#from src.main import func
 import sys
 print(sys.path)
 
