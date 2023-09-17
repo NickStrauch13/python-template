8f27ed0769bab7a78e916dbe1c0e9b15abb8033c
Nick Strauch <61529274+NickStrauch13@users.noreply.github.com>
2023-09-16 23:52:48 -0400

    Update setup.sh user

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-source /home/codespace/venv/bin/activate
+source /home/vscode/venv/bin/activate
 # Append it to bash so every shell launches with it 
-echo 'source /home/codespace/venv/bin/activate' >> ~/.bashrc
+echo 'source /home/vscode/venv/bin/activate' >> ~/.bashrc
