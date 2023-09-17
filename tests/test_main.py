9d8a3f8e771c4e6e749b46e0213876d56a96258b
Nick Strauch <61529274+NickStrauch13@users.noreply.github.com>
2023-09-17 16:38:26 +0000

    added src to path

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,7 +1,9 @@
-from src.main import func
-
+#from src.main import func
+import sys
+print(sys.path)
 
 def test_func():
     assert func(3) == 4
     assert func(4) == 5
     assert func(-1) == 0
+
