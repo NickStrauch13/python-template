da77ff509fef4bf1ce07d1544c5ed62892f45e4b
Nick Strauch <61529274+NickStrauch13@users.noreply.github.com>
2023-10-21 17:53:23 +0000

    Changed default user. Fixing permission error.

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-source /home/codespace/venv/bin/activate
+source /home/vscode/venv/bin/activate
 #append it to bash so every shell launches with it 
-echo 'source /home/codespace/venv/bin/activate' >> ~/.bashrc
+echo 'source /home/vscode/venv/bin/activate' >> ~/.bashrc
 
